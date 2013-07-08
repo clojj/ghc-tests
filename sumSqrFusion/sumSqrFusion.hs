df96046eddfa210666f4c07d19838e0105f4ce00
Jan Stolarek <jan.stolarek@p.lodz.pl>
2013-07-08 13:36:09 +0100

    Remove main from sumSqrFusion

--- sumSqrFusion/sumSqrFusion.hs
+++ sumSqrFusion/sumSqrFusion.hs
@@ -1,4 +1,6 @@
+module SumSqrFusion where
+
 import qualified Data.Vector as U
 
-main = print $ U.sum $ U.map (\x -> x * x) v
+f = U.sum $ U.map (\x -> x * x) v
     where v = U.enumFromTo 1 (1000000000 :: Int)
