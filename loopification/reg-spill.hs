9a8e3e22a0dc184467a75d2992182c00201aa66b
Jan Stolarek <jan.stolarek@p.lodz.pl>
2013-07-17 11:59:01 +0000

    Update programs for loopification

--- loopification/reg-spill.hs
+++ loopification/reg-spill.hs
@@ -3,6 +3,6 @@ module Spill where
 
 import GHC.Exts
 
-z :: Int# -> Int# -> Int# -> Int# -> Int# -> Int# -> Int# -> Int# -> Int# -> Int# -> Int# -> Int# -> Int# -> Int# -> Int# -> Int# -> Int#
-z !a 0# _ _ _ _ _ _ _ _ _ _ _ _ _ _ = a
-z !a !b !c !d !e !f !g !h !i !j !k !l !m !n !o !p = z (a *# c) (b -# 1#) (d +# 1#) (e +# 1#) (f *# 2#) (g *# 2#) (h +# 2#) (i +# 2#) (j *# 2#) (k +# 3#) (l *# 3#) (m +# 2#) n o p c
+f :: Float# -> Int# -> Float#
+f a 0# = a
+f a n  = f (a `timesFloat#` a) (n -# 1#)
