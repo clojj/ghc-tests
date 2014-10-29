cc7ca6f54ef97898097fca8780620eee5b75168e
Jan Stolarek <jan.stolarek@p.lodz.pl>
2014-10-29 08:12:40 +0100

    Tests for T6018

--- types/T6018/T6018.hs
+++ types/T6018/T6018.hs
@@ -1,16 +1,42 @@
-{-# LANGUAGE TypeFamilies, PolyKinds #-}
+{-# LANGUAGE TypeFamilies, PolyKinds, DataKinds, KindSignatures, TypeOperators, MultiParamTypeClasses #-}
+{- LANGUAGE AllowAmbiguousTypes #-}
 module T6018 where
 
-{-
-type family F a b c | result -> a b c
-type instance F Int  Char Bool = Bool
-type instance F Char Bool Int  = Int
-type instance F Bool Int  Char = Char
+import T6018a
 
-type family G a b c | result -> a b
+
+--type family F a b c = (result :: *) | result -> a b c
+--type instance F Int  Char Bool = Bool
+--type instance F Char Bool Int  = Int
+
+type instance F Bool Int  Char = Int
+
+{-
+type family D a b c = (result :: *) | result -> a b c
+type instance D Char Bool Int  = Bool
+type instance D Char Bool Int  = Int
+type instance D Bool Int  Char = Int
+
+-}
+{-
+
+type family Fc a b c = a | a -> a b where
+    Fc Int  Char Bool = Bool
+    Fc Char Bool Int  = Int
+    Fc Bool Int  Char = Char
+
+class Fcl a b c where
+    type Foo a b = r | r -> a b
+
+type family G a b c = a | a -> a b c
 type instance G Int  Char Bool = Bool
 type instance G Int  Char Int  = Bool
 type instance G Bool Int  Int  = Int
+
+type family Gc a b c = a | a -> a b c where
+    Gc Int  Char Bool = Bool
+    Gc Int  Char Int  = Bool
+    Gc Bool Int  Int  = Int
 
 type family H a b c | result a -> b c, result b -> a c, result c -> a b
 type instance H Int  Char   Double = Int
@@ -95,14 +121,48 @@ f _ _ = False
 -}
 
 {-
-type family F a | result -> a where
+type family F a = r | r -> a where
     F a = a
 
-foo :: F a -> F a
-foo = id
+type family G a = r | r -> a where
+    G Int = Int
+    G Bool = Bool
+    G a = a
 -}
 
 {-
+type family Map (f :: k -> l) (ks :: [k]) :: [l] where
+    Map f '[] = '[]
+    Map f (x ': xs) = f x ': Map f xs
+
+data Proxy a = Proxy
+
+foo :: Proxy (Map Maybe [Int, Char])
+foo = undefined
+
+
+Map f [a, b] ~ [Maybe Int, Maybe Char]
+
+
+type family K a = r | r -> a where
+    K Int = Bool
+    K Bool = Int
+    K a = a
+-}
+--    F a = a
+--    F Bool = Bool
+--    F a = a
+{-
+
+foo :: F a -> F a
+foo = id
+
+bar :: Int -> Int
+bar = foo
+
+
+
+
 type family F a | result -> a where
     F Char = Bool
     F Bool = Int
@@ -110,4 +170,10 @@ type family F a | result -> a where
 
 idChar :: (F a ~ Bool) => a -> Char
 idChar a = a
+
+data N = Z | S N
+
+type family P (a :: N) (b :: N) = r | r -> a b where
+    P Z n = n
+    P (S n) m = S (P n m)
 -}
