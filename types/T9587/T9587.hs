5f897e0deb599a44009c158cd35905b9ee046fde
Jan Stolarek <jan.stolarek@p.lodz.pl>
2015-09-20 11:42:37 +0200

    Update test code for GHC Trac #9587

--- types/T9587/T9587.hs
+++ types/T9587/T9587.hs
@@ -2,7 +2,6 @@
 {-# LANGUAGE TypeFamilies #-}
 {-# LANGUAGE KindSignatures #-}
 {-# LANGUAGE ScopedTypeVariables #-}
--- {-# LANGUAGE AllowAmbiguousTypes #-}
 
 module T9587 where
 
@@ -15,26 +14,5 @@ class ESymantics repr where
     lam :: (repr a -> repr b) -> repr (Arr repr a b)
     app :: repr (Arr repr a b) -> repr a -> repr b
 
-{-
-te4 :: (Arr repr (Arr repr Int Int) (Arr repr Int Int)
-        ~
-      Arr repr (Arr repr Int Int) (Arr repr Int b),
-      ESymantics repr) =>
-     repr b
--}
 te4 = let c3 = lam (\f -> lam (\x -> f `app` (f `app` (f `app` x))))
       in (c3 `app` (lam (\x -> x `add` int 14))) `app` (int 0)
-
-
--- t = lam (\f -> f `app` int 0)
-
-newtype R a = R{unR :: a}
-type instance Arr R a b = a -> b
-
-instance ESymantics R where
-  int = R
-  add (R x) (R y) = R $ x + y
-  lam f = R $ unR . f . R
-  app (R f) (R x) = R $ f x
-
-tR = unR te4
