ec8420266ce403334b5e804a178bc79fecb10f58
Jan Stolarek <jan.stolarek@p.lodz.pl>
2015-11-10 07:54:41 +0100

    Remove tests for #11007

--- types/T11007/A.hs
+++
@@ -1,8 +0,0 @@
-{-# LANGUAGE TypeFamilies #-}
-
-module A where
-
-import B
-
-type family Id x = r | r -> x where
-  Id a = a
--- types/T11007/A.hs-boot
+++
@@ -1,5 +0,0 @@
-{-# LANGUAGE TypeFamilies #-}
-
-module A where
-
-type family Id x = r | r -> x where ..
