4623effb585dcccff3c8bd06c32f8d66e902df1e
Jan Stolarek <jan.stolarek@p.lodz.pl>
2015-12-15 11:46:37 +0100

    Tests for #10982

--- types/T10982/T10982.hs
+++ types/T10982/T10982.hs
@@ -1,8 +1,15 @@
-{-# LANGUAGE TypeFamilies #-}
-{- # LANGUAGE NamedWildCards #-}
+{-# LANGUAGE TypeFamilies, PolyKinds #-}
+{-# LANGUAGE NamedWildCards #-}
+{-# LANGUAGE PartialTypeSignatures, DatatypeContexts #-}
 
 module T10982 where
 
-type family F a where
+{-
+type family F (_a :: k) where
     F _t = Int
 
+foo :: a -> a
+foo _a = _a
+-}
+
+data (Eq _a) => Foo _a = Foo { getFoo :: _a }
