3ed4afafe5c8024ab75ad9ddd255a082f1fe06d9
Jan Stolarek <jan.stolarek@p.lodz.pl>
2015-12-16 13:50:57 +0100

    Update tests for #10982

--- types/T10982/T10982.hs
+++ types/T10982/T10982.hs
@@ -13,3 +13,31 @@ foo _a = _a
 -}
 
 data (Eq _a) => Foo _a = Foo { getFoo :: _a }
+
+type Synonym _a = _a -> _a
+-- "Unexpected type '_a' In the type declaration for 'Synonym'"
+
+data A a _b = ACon a a
+-- "Unexpected type '_b' In the data declaration for 'A'"
+
+data B _a b = BCon _a _a
+-- "Unexpected type '_a' In the data declaration for 'B'"
+-}
+type family C a (b :: _k) where
+    C a b = a -> a
+-- "Wildcard '_b' not allowed in a type pattern of family instance for 'C'"
+{-
+type family D a b where
+  D _a b = _a -> _a
+-- "Wildcard '_a' not allowed in a type pattern of family instance for 'D'"
+-- "Wildcard '_a' not allowed in the declaration for type synonym 'D'" (twice)
+
+data family E a b
+data instance E a _b = ECon a a
+-- "Wildcard '_b' not allowed in a type pattern of family instance for 'E'"
+
+data family F a b
+data instance F _a b = FCon _a _a
+-- "Wildcard '_a' not allowed in a type pattern of family instance for 'F'"
+-- "Wildcard '_a' not allowed in the definition of data constructor 'FCon'" (twice)
+-}
